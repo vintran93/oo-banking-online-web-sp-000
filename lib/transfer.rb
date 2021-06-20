6fb94a1aa3185e0a890e84894f846f15eb4e1ecd
Vincent Tran <vincentuong@yahoo.com>
2021-06-20 19:58:13 +0000

    Automatically backed up by Learn

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -19,6 +19,7 @@ class Transfer
   end
   
   def execute_transaction
-    if valid? @@ sender.balance > amount && self.status == ""
+    if valid? @@ sender.balance > amount && self.status == "pending"
+      
   
 end
