7f328a4fbd0cc7fd64e57eee87fe38eb0ac9e446
Vincent Tran <vincentuong@yahoo.com>
2021-06-20 20:03:26 +0000

    Automatically backed up by Learn

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -37,5 +37,11 @@ class Transfer
         reject_transfer
     end
   end
-    
+  
+  def reject_transfer
+   self.status = "rejected"
+   "Transaction rejected. Please check your account balance."
+ end
+end
+
 end
