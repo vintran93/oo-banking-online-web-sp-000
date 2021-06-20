daf112c13c6296622e7a05a839a93412a9cf7f7c
Vincent Tran <vincentuong@yahoo.com>
2021-06-20 19:59:11 +0000

    Automatically backed up by Learn

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -19,11 +19,12 @@ class Transfer
   end
   
   def execute_transaction
-    if valid? @@ sender.balance > amount && self.status == "pending"
+    if valid? && sender.balance > amount && self.status == "pending"
       sender.balance -= amount
       receiver.balance += amount
     else
       reject_transfer
-      
+    end
+  end
   
 end
